32a4b1e20dd5cc008e986b6e5c68a8ff76544afa
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2024-01-09 17:13:01 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashComponent } from './page/dash/dash.component';
+import { LoginComponent } from './page/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'dash',
+    component:DashComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashComponent } from './page/dash/dash.component';
+import { NavComponent } from './comp/nav/nav.component';
+import { LoginComponent } from './page/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashComponent,
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
